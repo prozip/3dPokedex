7c475ab1bd33e2d661c025c272ba985821c1baf4
prozip <quachkimson2000@gmail.com>
2022-03-07 17:31:40 +0700

    fix gigan link

--- assets/js/pokedex.js
+++ assets/js/pokedex.js
@@ -6,8 +6,6 @@ var index_json = []
 var end = false
 
 var observer = new IntersectionObserver(function (entries) {
-    // isIntersecting is true when element and viewport are overlapping
-    // isIntersecting is false when element and viewport don't overlap
     if ((entries[0].isIntersecting === true) & (end == false))
         fetchPoke(index_json)
 }, { threshold: [0] });
